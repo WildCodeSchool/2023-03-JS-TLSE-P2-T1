32727dfae17cb46df8bd0b8bf7b9d71b9757e44e
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-04-21 12:22:20 +0200

    added style modification to navbar, began responsive design

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NavBar() {
+  const [filterMainName, setFilterMainName] = useState("Culture et Sport");
+  const [filterDateName, setFilterDateName] = useState("Flexible");
+  const [filterTagName, setFilterTagName] = useState(["Tag 1"]);
+
+  // use setFilterMainName to change the name of the main filter into "Culture et Sport"
+  // use setFilterDateName to change the name of the date filter into "Flexible"
+  // use setFilterTagName to change the name of the tag filter into "Tag 1"
+  setFilterDateName("Flexible");
+  setFilterTagName(["Tag 1", "Tag 2", "Tag 3"]);
+  setFilterMainName("Culture et Sport");
+
   return (
     <nav>
       <ul>
         {/* define a li containing logo and website name */}
         <li>
-          <img src="..\assets\myTouloulistLogo.png" alt="Logo MyTouloulist" />
+          <img src="../src/assets/myLogo.png" alt="Logo MyTouloulist" />
           <h1>myTouloulist</h1>
         </li>
         <li>
-          <button type="button">Filter</button>
+          <button type="button">
+            <p>{filterMainName}</p>
+            <p>{filterDateName}</p>
+            {/* map the filterTagName element to display p */}
+            {filterTagName.map((el) => (
+              <p>{el}</p>
+            ))}
+          </button>
         </li>
       </ul>
     </nav>
