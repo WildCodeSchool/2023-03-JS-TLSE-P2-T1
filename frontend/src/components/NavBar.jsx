3a3eb83f7b059b8acef233db5a1973d934cd48ee
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-04-22 14:54:22 +0200

    S02US10_NavBar Navbar completed, style declaration in mobile first on dedicated CSS

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
+import "./NavBar.css";
 
 function NavBar() {
   const [filterMainName, setFilterMainName] = useState("Culture et Sport");
@@ -20,11 +21,23 @@ function NavBar() {
         </li>
         <li>
           <button type="button">
-            <p>{filterMainName}</p>|<p>{filterDateName}</p>
-            {/* map the filterTagName element to display p */}
-            {filterTagName.map((el) => (
-              <p key={el}>| {el}</p>
-            ))}
+            <img
+              src="../src/assets/filter_icon.png"
+              alt="filter button icon"
+              id="filter_icon"
+            />
+            <div className="header_filters">
+              <div className="header_main_filters">
+                <p>{filterMainName} | </p>
+                <p>{filterDateName}</p>
+              </div>
+              <div className="header_tags_filters">
+                {/* map the filterTagName element to display p */}
+                {filterTagName.map((el) => (
+                  <p key={el}>| {el}</p>
+                ))}
+              </div>
+            </div>
           </button>
         </li>
       </ul>
