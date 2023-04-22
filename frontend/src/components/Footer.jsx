f64cc957ffc17ac6fa51040247c3f95b78ddcc1d
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-04-22 16:12:00 +0200

    footer finished, ready to receive modal to an about us page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,14 @@
-import React from "react";
 import "./Footer.css";
 
 function Footer() {
   return (
     <div>
-      {/* footer containing a p with "copyright 2023" on the left and a link "About us" on the right */}
       <footer>
         <p>© 2023</p>
-        <a href="www.google.fr">About us</a>
+        {/* add <p onClick={handleAboutUsClick} className="about-us"> with a key event listener */}
+        <button type="button" className="about-us">
+          About us
+        </button>
       </footer>
     </div>
   );
