10d4fe49721a9c0a3fd082df9c1f8df1bca334e1
GaspardCLD <115171560+GaspardCLD@users.noreply.github.com>
2023-04-27 10:52:38 +0200

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,6 @@ function Footer() {
     <div>
       <footer>
         <p>© myTouloulist 2023</p>
-        {/* add <p onClick={handleAboutUsClick} className="about-us"> with a key event listener */}
         <button type="button" className="about-us">
           About us
         </button>
