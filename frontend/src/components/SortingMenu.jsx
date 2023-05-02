5c0edeafc5a7dcb23b560362afc770d6e610dd14
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-05-02 10:35:10 +0200

    sorting menu development

--- frontend/src/components/SortingMenu.jsx
+++ frontend/src/components/SortingMenu.jsx
@@ -3,10 +3,68 @@ import PropTypes from "prop-types";
 import "./SortingMenu.css";
 
 function SortingMenu({ finalResult, setFinalResult }) {
-  setFinalResult(...finalResult);
   return (
     <div>
-      <p>Clique ici pour trier</p>
+      <p>Trier par : </p>
+      <select
+        className="sorting-menu"
+        onChange={(event) => {
+          if (event.target.value === "alphabetical") {
+            setFinalResult(
+              finalResult.sort((a, b) => {
+                if (a.nature < b.nature) {
+                  return -1;
+                }
+                if (a.nature > b.nature) {
+                  return 1;
+                }
+                return 0;
+              })
+            );
+          } else if (event.target.value === "reverse-alphabetical") {
+            setFinalResult(
+              finalResult.sort((a, b) => {
+                if (a.nature < b.nature) {
+                  return 1;
+                }
+                if (a.nature > b.nature) {
+                  return -1;
+                }
+                return 0;
+              })
+            );
+          } else if (event.target.value === "most-recent") {
+            setFinalResult(
+              finalResult.sort((a, b) => {
+                if (a.startingDate < b.startingDate) {
+                  return 1;
+                }
+                if (a.startingDate > b.startingDate) {
+                  return -1;
+                }
+                return 0;
+              })
+            );
+          } else if (event.target.value === "oldest") {
+            setFinalResult(
+              finalResult.sort((a, b) => {
+                if (a.startingDate < b.startingDate) {
+                  return -1;
+                }
+                if (a.startingDate > b.startingDate) {
+                  return 1;
+                }
+                return 0;
+              })
+            );
+          }
+        }}
+      >
+        <option value="alphabetical">Ordre alphabétique</option>
+        <option value="reverse-alphabetical">Ordre alphabétique inverse</option>
+        <option value="most-recent">Plus récent</option>
+        <option value="oldest">Plus ancien</option>
+      </select>
     </div>
   );
 }
