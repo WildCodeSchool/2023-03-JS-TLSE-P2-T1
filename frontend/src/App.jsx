72e2a83377bd27408049c51fb26dcd6c485eb42c
Jean-Nicaise <118432114+jeannicaise@users.noreply.github.com>
2023-05-03 15:17:04 +0200

    correction address

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -81,7 +81,7 @@ function App() {
             coordinates: el.fields.geo_point,
             // defining the address result as 'XX rue de XXX, 31XXX SOMECITY"
             address: `${
-              el.fields.lieu_addresse_2
+              el.fields.lieu_adresse_2
             }, ${el.fields.code_postal.toString()} ${el.fields.commune}`,
             // defining the tags result as an array of tags, split by comma, from el.fields.theme_de_la_manifestation, only if it exists
             tags:
@@ -129,7 +129,7 @@ function App() {
             coordinates: el.fields.geo_point_2d,
             // getting address from API. If it doesn't exist, developer must write a condition that returns sector instead
             address: `${
-              el.fields.ins_addresse ? `${el.fields.ins_addresse} ,` : ""
+              el.fields.ins_adresse ? `${el.fields.ins_adresse} ,` : ""
             }${el.fields.ins_codepostal.toString()}`,
             tags: ["Stade"],
           }));
