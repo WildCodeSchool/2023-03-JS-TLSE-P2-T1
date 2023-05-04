e44feec1311226f4cd098365ff32ceaccaa24398
Gaspard Caillaud <gaspard.caillaud@gmail.com>
2023-05-04 16:12:44 +0200

    created Error component

---
+++ frontend/src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Error.css";
+
+function Error() {
+  return (
+    <div className="error">
+      <p>Le site a reçu trop de requêtes pour aujourd'hui, à demain !</p>
+    </div>
+  );
+}
+
+export default Error;
